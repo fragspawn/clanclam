0adf46899dae62f045c143b8e24418f942911fdf
developer <developer@webserver.tafeqld.dd>
2017-03-07 12:20:55 +1000

    debug Web Services

--- index.php
+++ index.php
@@ -3,11 +3,19 @@
 
     include './lib/database.php';
     include './lib/auth.php';
+    include './lib/steamservice.php';
     include './vue/ui.php';
+
 
     check_session();
     print_header();
     print_menu();
+
+
+    // body goes here:
+    
+   
+  
     print_footer();
 
 ?>
--- steamid.php
+++ steamid.php
@@ -7,16 +7,18 @@
 // steam_ID frae_spawn  = 76561197974169039
     $steam_info = get_user_info_from_steam('76561197974169039');
 
-/* PRINT 1st PLAYER DATA 
-    foreach($steam_info['response']['players'][0] as $key=>$item) {
+//PRINT 1st PLAYER DATA 
+/*    foreach($steam_info['response']['players'][0] as $key=>$item) {
         echo $key . ' ' . $item . ',';
     }
-    echo $steam_info['response']['players'][0]['primaryclanid'];
-*/
+    personaname, avatar
+ */
 
 // GET CLAN INFO IN XML
     if(isset($steam_info['response']['players'][0]['primaryclanid'])) {
         $clan_xml = get_clan_info_from_steam($steam_info['response']['players'][0]['primaryclanid']);
+        echo $steam_info['response']['players'][0]['personaname'];
+        echo '<img src="' . $steam_info['response']['players'][0]['avatar'] . '"/>';
     } else {
         echo 'no clan';
         die();
@@ -26,8 +28,10 @@
     foreach($clan_xml->members->steamID64 as $steamID) {
         $steam_clan_info = get_user_info_from_steam($steamID);
         if(isset($steam_clan_info['response']['players'][0]['primaryclanid'])) {
-            echo $steam_clan_info['response']['players'][0]['primaryclanid'];
-            echo '<br>';
+            echo $steam_clan_info['response']['players'][0]['personaname'] . ' ';
+            if($steam_info['response']['players'][0]['primaryclanid'] != $steam_clan_info['response']['players'][0]['primaryclanid']) {
+                echo '*';
+            }
         }
     } 
 
